8520b1d8911bf9d1e81d1764f086eab5da9ff375
mgv <mglasner10@gmail.com>
2023-03-23 22:41:51 -0300

    Texts in home page

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,8 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Ingeniero de software</p>
+        <p>Blog personal</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
